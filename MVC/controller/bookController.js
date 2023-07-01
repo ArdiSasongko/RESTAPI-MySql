fcbd68379805c928cc4871cc96a9cd6a9cd0ef2d
ArdiSasongko <ardisasongko002@gmail.com>
2023-07-01 18:06:56 +0700

    add Routes dan Controllers

--- MVC/controller/bookController.js
+++ MVC/controller/bookController.js
@@ -65,6 +65,85 @@ const insertBook = async (req, res) => {
       return res.status(httpStatus.BAD_REQUEST).json(response);
     }
   };
+
+  const updateBook = async (req,res) =>{
+    try {
+      const { id } = req.params
+      const { title, description, price, amount } = req.body
+
+      let update = []
+      let values = []
+
+      if(title){
+        update.push("title = ?")
+        values.push(title)
+      }
+
+      if(description){
+        update.push("description = ?")
+        values.push(description)
+      }
+
+      if(price){
+        update.push("price = ?")
+        values.push(price)
+      }
+
+      if(amount){
+        update.push("amount = ?")
+        values.push(amount)
+      }
+
+      if(!id){
+        const response = new Response.Error(true, "Cant Find Id")
+        return res.status(httpStatus.BAD_REQUEST).json(response)
+      }else{
+        const sqlFindBook = "SELECT * FROM books WHERE id_book = ?";
+        const book = await query(sqlFindBook, [id]);
+
+        if (book.length === 0) {
+          const response = new Response.Error(true, "Book not found");
+          return res.status(httpStatus.BAD_REQUEST).json(response);
+        }
+        const updateString = update.join(", ")
+        const sql = `UPDATE books SET ${updateString} WHERE id_book = ?`
+        values.push(id)
+
+        await query(sql,values)
+        const updatedBook = { ...book[0], ...req.body };
+        const response = new Response.Success(false, "Success Update Book", updatedBook);
+        return res.status(httpStatus.OK).json(response);
+      }
+      
+    } catch (error) {
+      const response = new Response.Error(true, error.message);
+      return res.status(httpStatus.OK).json(response);
+    }
+  }
+
+  const deleteBook = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const sqlFindBook = `SELECT * FROM books WHERE id_book = ?`
+      const book = await query(sqlFindBook, [id])
+
+      if(book.length === 0){
+        const response = new Response.Error(true, "Book not Found")
+        return res.status(httpStatus.BAD_REQUEST).json(response)
+      }else{
+        const sql = `DELETE FROM books WHERE id_book = ?`;
+        const values = [id];
+        await query(sql, values);
+        
+        const response = new Response.Success(false, "Success Delete"); 
+        return res.status(httpStatus.OK).json(response);
+      }
+      
+    } catch (error) {
+      const response = new Response.Error(true, error.message);
+      return res.status(httpStatus.OK).json(response);
+    }
+  }
   
 
-module.exports = { getBooks, getBook, insertBook };
+module.exports = { getBooks, getBook, insertBook, updateBook, deleteBook };
--- MVC/router/bookRouter.js
+++ MVC/router/bookRouter.js
@@ -5,5 +5,7 @@ const book = require("../controller/bookController")
 router.get("/", book.getBooks)
 router.get("/:id", book.getBook)
 router.post("/", book.insertBook)
+router.put("/:id", book.updateBook)
+router.delete("/:id", book.deleteBook)
 
 module.exports = router
